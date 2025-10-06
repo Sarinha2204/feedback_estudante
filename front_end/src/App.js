f2eabd95d9cd59911c17bc524da7f602928b5610
vinyok <vmarconsin@gmail.com>
2025-10-05 22:22:09 -0300

    implementando REACT e construindo a tela de login.

---
+++ front_end/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import LoginScreen from './components/LoginScreen/Login_screen';
+
+function App() {
+  return (
+    <div>
+      <LoginScreen />
+    </div>
+  );
+}
+
+export default App;
